306cdda3e02c448435e56aa863b9bede548bdc80
lolabulkin <54717937+lolabulkin@users.noreply.github.com>
2021-02-25 18:45:09 -0800

    bulkinfamilymembers

--- familycode.py
+++ familycode.py
@@ -11,44 +11,44 @@ fm5 = { "name":"Nelly", "age":47, "sport":"yoga", "color":"red", "parent":True}
 fm6 = { "name":"Anatoly", "age":54, "sport":"biking", "color":"purple", "parent":True}
 
 #Turn the dictionaries into a list.
-list_of_family_members = [fm1, fm2, fm3, fm4, fm5, fm6]
+list_of_bulkinfamily_members = [fm1, fm2, fm3, fm4, fm5, fm6]
 print("List Of Family Members")
-print(type(list_of_family_members))
-print(list_of_family_members)
+print(type(list_of_bulkinfamily_members))
+print(list_of_bulkinfamily_members)
 print()
-for family_member in list_of_family_members:
+for family_member in list_of_bulkinfamily_members:
     print(family_member['name'] + ", " + str(family_member['age']) + ", " + family_member['sport'] + ", " + family_member['color'])
 print()
 #Turn the list into a dictionary called 'family'
-family = {'family_members': list_of_family_members}
+family = {'bulkinfamily_members': list_of_bulkinfamily_members}
 #Print members to run/console
 print("Dictionary Of Family Members")
 print(type(family))
 print(family)
-family_members = family['family_members']
+bulkinfamily_members = family['bulkinfamily_members']
 print()
-for family_member in family_members:
-    print(family_member['name'] + ", " + str(family_member['age']) + ", " + family_member['sport'] + ", " + family_member['color'])
+for bulkinfamily_member in bulkinfamily_members:
+    print(bulkinfamily_member['name'] + ", " + str(bulkinfamily_member['age']) + ", " + bulkinfamily_member['sport'] + ", " + bulkinfamily_member['color'])
 print()
 #Turn the dictionary into a JSON file
-json_family_members = json.dumps(family)
+json_bulkinfamily_members = json.dumps(family)
 #Print the JSON file to the run/console window, inserting a character or emoji in between each item in the JSON.
 print("JSON Family Members")
-print(type(json_family_members))
-print(json_family_members)
-x = json.loads(json_family_members)
-family_members = x['family_members']
+print(type(json_bulkinfamily_members))
+print(json_bulkinfamily_members)
+x = json.loads(json_bulkinfamily_members)
+family_members = x['bulkinfamily_members']
 print()
-for family_member in family_members:
+for family_member in bulkinfamily_members:
     print(family_member['name'] + ", " + str(family_member['age']) + ", " + family_member['sport'] + ", " + family_member['color'])
     print(':)')
 print()
-#Return JSON file to a dictionary with key "family"- my key is family_members
-print("Return JSON File To A Dictionary - Key is family_members")
-print(json_family_members)
+#Return JSON file to a dictionary with key "family"- my key is bulkinfamily_members
+print("Return JSON File To A Dictionary - Key is bulkinfamily_members")
+print(json_bulkinfamily_members)
 #Using program logic, make new dictionaries from "family" key/values, creating "parents" and "siblings" key with logically corresponding lists as values.
-dictfamilymembers = json.loads(json_family_members)
-listfamilymembers = dictfamilymembers['family_members']
+dictfamilymembers = json.loads(json_bulkinfamily_members)
+listfamilymembers = dictfamilymembers['bulkinfamily_members']
 parents = []
 siblings = []
 me = []
