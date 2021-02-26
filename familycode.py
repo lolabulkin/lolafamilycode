c2624db6d3f2dd41f2ab3d0ca03ad5805a6d0fc7
lolabulkin <54717937+lolabulkin@users.noreply.github.com>
2021-02-25 18:37:43 -0800

    Initial commit

---
+++ familycode.py
@@ -0,0 +1,83 @@
+# code here
+import json
+
+#other code
+#Make a Dictionary of you, your siblings, parents, and grandparents.  Have at least 4 attributes per person.
+fm1 = { "name":"Lola", "age":16, "sport":"soccer", "color":"pink", "notatrisk":True}
+fm2 = { "name":"Sophie", "age":16, "sport":"soccer", "color":"purple", "sibling":True, "notatrisk":True}
+fm3 = { "name":"Alex", "age":25, "sport":"basketball", "color":"blue", "sibling":True, "notatrisk":True}
+fm4 = { "name":"Michael", "age":23, "sport":"wrestling", "color":"blue", "sibling":True}
+fm5 = { "name":"Nelly", "age":47, "sport":"yoga", "color":"red", "parent":True}
+fm6 = { "name":"Anatoly", "age":54, "sport":"biking", "color":"purple", "parent":True}
+
+#Turn the dictionaries into a list.
+list_of_family_members = [fm1, fm2, fm3, fm4, fm5, fm6]
+print("List Of Family Members")
+print(type(list_of_family_members))
+print(list_of_family_members)
+print()
+for family_member in list_of_family_members:
+    print(family_member['name'] + ", " + str(family_member['age']) + ", " + family_member['sport'] + ", " + family_member['color'])
+print()
+#Turn the list into a dictionary called 'family'
+family = {'family_members': list_of_family_members}
+#Print members to run/console
+print("Dictionary Of Family Members")
+print(type(family))
+print(family)
+family_members = family['family_members']
+print()
+for family_member in family_members:
+    print(family_member['name'] + ", " + str(family_member['age']) + ", " + family_member['sport'] + ", " + family_member['color'])
+print()
+#Turn the dictionary into a JSON file
+json_family_members = json.dumps(family)
+#Print the JSON file to the run/console window, inserting a character or emoji in between each item in the JSON.
+print("JSON Family Members")
+print(type(json_family_members))
+print(json_family_members)
+x = json.loads(json_family_members)
+family_members = x['family_members']
+print()
+for family_member in family_members:
+    print(family_member['name'] + ", " + str(family_member['age']) + ", " + family_member['sport'] + ", " + family_member['color'])
+    print(':)')
+print()
+#Return JSON file to a dictionary with key "family"- my key is family_members
+print("Return JSON File To A Dictionary - Key is family_members")
+print(json_family_members)
+#Using program logic, make new dictionaries from "family" key/values, creating "parents" and "siblings" key with logically corresponding lists as values.
+dictfamilymembers = json.loads(json_family_members)
+listfamilymembers = dictfamilymembers['family_members']
+parents = []
+siblings = []
+me = []
+for member in listfamilymembers:
+    if "parent" in member:
+        parents.append(member)
+    elif "sibling" in member:
+        siblings.append(member)
+    else:
+        me.append(member)
+#Print members to run/console in fashion that highlights
+print()
+print("Organized Members")
+print()
+print("Parents")
+print(parents)
+print()
+for member in parents:
+    print(member['name'] + ", " + str(member['age']) + ", " + member['sport'] + ", " + member['color'])
+print()
+print("Siblings")
+print(siblings)
+print()
+for member in siblings:
+    print(member['name'] + ", " + str(member['age']) + ", " + member['sport'] + ", " + member['color'])
+print()
+print("Me")
+print(me)
+print()
+for member in me:
+    print(member['name'] + ", " + str(member['age']) + ", " + member['sport'] + ", " + member['color'])
+print()
